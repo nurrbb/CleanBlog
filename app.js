bca1c40521a1e491a3273f80aa7c0a4151ed8cb2
nurrbb <enurb320@gmail.com>
2025-12-06 16:41:06 +0300

    Set up public static folder and configure EJS view engine by migrating HTML files to .ejs in the views directory

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.set('view engine', 'ejs');
 
-  const photo = {
-    id: 1,
-    name: "Photo Name",
-    description: "Photo description"
-  }
-  res.send(photo)
-})
+const myLogger = (req, res, next) => {
+  console.log('Middleware log 001');
+  next();
+};
+
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware log 001');
+  next();
+};
+
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 const port = 3000;
 app.listen(port, () => {
