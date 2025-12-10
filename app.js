688dc6a6506c9cbbd9596b86b44ad6665f46ad22
nurrbb <enurb320@gmail.com>
2025-12-10 11:54:52 +0300

    Connect MongoDB and render blog posts dynamically

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
 
+const Post = require('./models/Post'); 
+
 const app = express();
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/cleanblog-test-db')
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('DB error:', err));
 
 app.set('view engine', 'ejs');
 
-const myLogger = (req, res, next) => {
-  console.log('Middleware log 001');
-  next();
-};
-
-const myLogger2 = (req, res, next) => {
-  console.log('Middleware log 001');
-  next();
-};
-
+// MIDDLEWARES
 app.use(express.static('public'));
-app.use(myLogger);
-app.use(myLogger2);
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+// ROUTES
+app.get('/', async (req, res) => {
+  const posts = await Post.find({}).sort('-dateCreated');
+  res.render('index', { posts });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body); /
+  res.redirect('/');
 });
 
 const port = 3000;
