8d6e540338f9186c94a69ba7c6ec7d930a0ab08e
nurrbb <enurb320@gmail.com>
2025-12-13 11:45:06 +0300

    Refactor app to MVC controllers: split page routes and post CRUD into pageController and postController

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejs = require('ejs');
-const path = require('path');
 
-const Post = require('./models/Post');
+const postController = require('./controllers/postController');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
@@ -30,51 +30,17 @@ app.use(
 
 // ROUTES
 
-// HOME - LIST POSTS
-app.get('/', async (req, res) => {
-  const posts = await Post.find({}).sort('-dateCreated');
-  res.render('index', { posts });
-});
+// Posts CRUD
+app.get('/', postController.getAllPosts);
+app.get('/posts/:id', postController.getPost);
+app.post('/posts', postController.createPost);
+app.put('/posts/:id', postController.updatePost);
+app.delete('/posts/:id', postController.deletePost);
 
-// ABOUT
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-// ADD POST PAGE
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-
-// CREATE POST
-app.post('/posts', async (req, res) => {
-  await Post.create(req.body);
-  res.redirect('/');
-});
-
-// SINGLE POST PAGE
-app.get('/posts/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render('post', { post });
-});
-
-// EDIT PAGE (UPDATE FORM)
-app.get('/posts/edit/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render('edit_post', { post });
-});
-
-// UPDATE POST (PUT)
-app.put('/posts/:id', async (req, res) => {
-  await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.redirect(`/posts/${req.params.id}`);
-});
-
-// DELETE POST (DELETE)
-app.delete('/posts/:id', async (req, res) => {
-  await Post.findByIdAndDelete(req.params.id);
-  res.redirect('/');
-});
+// Pages
+app.get('/about', pageController.getAboutPage);
+app.get('/add_post', pageController.getAddPostPage);
+app.get('/posts/edit/:id', pageController.getEditPostPage);
 
 const port = 3000;
 app.listen(port, () => {
